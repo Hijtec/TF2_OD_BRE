f418b02fc188e571a38895f9ee93deaaf64752c7
cernil <martin.cernil@ysoft.com>
2021-02-10 14:50:38 +0100

    flags for elevator controls detection added

--- src/main/flags_global.py
+++ src/main/flags_global.py
@@ -14,7 +14,7 @@ flags.DEFINE_integer('age', None, 'Your age in years.', lower_bound=0)
 flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
 flags.DEFINE_enum('job', 'running', ['running', 'stopped'], 'Job status.')
 """
-####
+#####
 # INPUT FEED FLAGS START
 # common
 flags.DEFINE_enum('image_input_mode', 'camera', ['camera', 'video', 'folder'], 'Source of image data.')
@@ -30,6 +30,21 @@ flags.DEFINE_string('image_input_folder_path', None, 'PATH to the folder with im
 flags.DEFINE_enum('robot_position', 'elevator', ['elevator', 'hall', 'reserved'], 'Current location of the robot.')
 
 # INPUT FEED FLAGS END
-####
+#####
+# ELEVATOR CONTROLS DETECTION FLAGS START
+# elevator element detection
+flags.DEFINE_string('detector_elements_model_path', None,
+                    'PATH to a SavedModel file capable of detection of elevator elements.')
+flags.DEFINE_enum('detection_elements_model_type', 'tf2',
+                  ['tf2', 'reserved'], 'Type of detection model used - RESERVED.')
+# floor button recognition
+flags.DEFINE_string('classification_floor_button_model_path', None,
+                    'PATH to a SavedModel file capable of classification of elevator floor buttons.')
+flags.DEFINE_enum('classification_floor_button_model_type', 'keras',
+                  ['keras', 'reserved'], 'Type of classification model used - RESERVED.')
+# ELEVATOR CONTROLS DETECTION FLAGS END
+#####
+
+
 
 FLAGS = flags.FLAGS
