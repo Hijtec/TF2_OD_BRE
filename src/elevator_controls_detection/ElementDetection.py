5163273ec8297e863cebbe97223736e1d0801bd3
cernil <martin.cernil@ysoft.com>
2021-02-10 10:58:28 +0100

    Detector object protos

---
+++ src/elevator_controls_detection/ElementDetection.py
@@ -0,0 +1,8 @@
+"""A script handling the Element Detection .
+This module contains a Class handling the inputs into the system
+"""
+from absl import logging
+from src.main.flags_global import FLAGS
+import src.object_detection.inference
+
+class
---
+++ src/object_detection/inference/Detector.py
@@ -0,0 +1,18 @@
+"""Abstract class Detector.
+This module contains a wrapper class around neural network inference.
+"""
+from abc import ABC, abstractmethod
+
+
+class Detector(ABC):
+    @abstractmethod
+    def load_detector(self, detector_path):
+        pass
+
+    @abstractmethod
+    def infer_tensor_input(self, tensor_input):
+        pass
+
+    @abstractmethod
+    def visualize_output(self):
+        pass
