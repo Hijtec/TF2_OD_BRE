a43aa7cc86c34a6c0e848dc7779468bc627a9dbe
cernil <martin.cernil@ysoft.com>
2021-02-11 23:56:07 +0100

    added floorbuttonclassification, roi extraction

--- src/main/RecognizeElevatorElements.py
+++ src/main/RecognizeElevatorElements.py
@@ -8,13 +8,17 @@ from src.main.flags_global import FLAGS
 from absl import logging
 
 from src.utils.detection_utils import non_max_suppress_detections, filter_one_category_from_detections_nms, create_category_index
+from src.utils.image_utils import crop_multiple_images_by_bndbox
+from matplotlib.pyplot import imshow, show
+import tensorflow as tf
+import numpy as np
 
 
 class RecognizeElevatorElements:
     def __init__(self):
         self.input = InputFeed()
         self.detection = ElementDetection()
-        # TODO: disabled for time saving self.classification = FloorButtonClassification()
+        self.classification = FloorButtonClassification()
         self.postprocessing = None  # TODO: these parts are not yet implemented
         self.visualization = None  # TODO: these parts are not yet implemented
 
@@ -28,7 +32,17 @@ class RecognizeElevatorElements:
         detections_nms = non_max_suppress_detections(detection_data)
         category_index = create_category_index(FLAGS.label_map_path_detection)
         detection_buttons = filter_one_category_from_detections_nms(detections_nms, category_index, category='btn_floor')
-        # TODO: filter out only floor buttons from the detection_data
+        detection_buttons_roi = crop_multiple_images_by_bndbox(input_data['ImageData'],
+                                                               detection_buttons['detection_boxes_nms'])
+        button_classifications = []
+        for button_roi in detection_buttons_roi:
+            button_roi_tensor = tf.convert_to_tensor(np.expand_dims(button_roi, 0), dtype=tf.float32)
+            button_roi_tensor_resized = tf.image.resize_with_pad(button_roi_tensor, 224, 224, method=tf.image.ResizeMethod.BICUBIC)
+            imshow(np.squeeze(button_roi_tensor_resized.numpy().astype(tf.int32)))
+            show()
+            self.classification.classify_next_tensor(button_roi_tensor_resized)
+            button_classification = self.classification.get_classification()
+            button_classifications.append(button_classification)
         return
 
     def loop(self):
