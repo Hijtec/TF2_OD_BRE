2ee277f172bab94e28e1cf4ea8e02f7836d214e2
cernil <martin.cernil@ysoft.com>
2021-02-11 16:53:52 +0100

    main app part 2, added filtering and processing of detection data

--- src/main/RecognizeElevatorElements.py
+++ src/main/RecognizeElevatorElements.py
@@ -4,15 +4,17 @@ TODO: Main class, describe what it does
 from src.elevator_controls_detection.ElementDetection import ElementDetection
 from src.elevator_controls_detection.FloorButtonClassification import FloorButtonClassification
 from src.input_feed.InputFeed import InputFeed
-
+from src.main.flags_global import FLAGS
 from absl import logging
+
+from src.utils.detection_utils import non_max_suppress_detections, filter_one_category_from_detections_nms, create_category_index
 
 
 class RecognizeElevatorElements:
     def __init__(self):
         self.input = InputFeed()
         self.detection = ElementDetection()
-        self.classification = FloorButtonClassification()
+        # TODO: disabled for time saving self.classification = FloorButtonClassification()
         self.postprocessing = None  # TODO: these parts are not yet implemented
         self.visualization = None  # TODO: these parts are not yet implemented
 
@@ -23,6 +25,9 @@ class RecognizeElevatorElements:
             return
         self.detection.detect_next_image(input_data['ImageData'])
         detection_data = self.detection.get_detection()
+        detections_nms = non_max_suppress_detections(detection_data)
+        category_index = create_category_index(FLAGS.label_map_path_detection)
+        detection_buttons = filter_one_category_from_detections_nms(detections_nms, category_index, category='btn_floor')
         # TODO: filter out only floor buttons from the detection_data
         return
 
