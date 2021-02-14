20b6de7549fee957170377d2a25e6584411fee03
cernil <martin.cernil@ysoft.com>
2021-02-14 23:58:52 +0100

    importing functions to be used by project visualization

---
+++ src/utils/visualization_utils.py
@@ -0,0 +1,2 @@
+from src.models.research.object_detection.utils.visualization_utils import save_image_array_as_png, \
+    draw_bounding_boxes_on_image_array, visualize_boxes_and_labels_on_image_array
