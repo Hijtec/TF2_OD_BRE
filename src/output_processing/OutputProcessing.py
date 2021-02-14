7c960cdb6d0a00e13f18e13dd19e18c9f9462e4e
cernil <martin.cernil@ysoft.com>
2021-02-14 23:57:47 +0100

    name tweak

--- src/output_processing/OutputProcessing.py
+++ src/output_processing/OutputProcessing.py
@@ -8,11 +8,11 @@ from absl import logging
 class OutputProcessing:
     def __init__(self, label_map_path):
         self.label_map_path = label_map_path
-        self.category_index = create_category_index(label_map_path)
+        self.category_index_detection = create_category_index(label_map_path)
 
     def filter_one_category(self, detections_nms, category):
         logging.info('Filtered one category from detections.')
-        return filter_one_category_from_detections_nms(detections_nms, self.category_index, category)
+        return filter_one_category_from_detections_nms(detections_nms, self.category_index_detection, category)
 
     def filter_by_nms(self, detections, max_output_size=100, iou_threshold=0.5, score_threshold=0.3):
         logging.info('Non-maximum suppression used on detections.')
