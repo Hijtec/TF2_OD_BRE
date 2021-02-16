a16720bdb1747cb9e139524bd61aa5025272dc8c
cernil <martin.cernil@ysoft.com>
2021-02-16 17:28:51 +0100

    optimized imports, pruning, logging

--- src/elevator_controls_detection/ElementDetection.py
+++ src/elevator_controls_detection/ElementDetection.py
@@ -1,12 +1,12 @@
 """A class handling the Element Detection.
 This module contains a Class capable of detecting elevator elements in an image.
 """
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 from absl import logging
-from src.models.research.object_detection.utils import label_map_util
-from src.object_detection.inference import DetectorTF2
+
 from src.main.flags_global import FLAGS
+from src.object_detection.inference import DetectorTF2
 
 
 class ElementDetection:
@@ -26,12 +26,14 @@ class ElementDetection:
             # TODO: more types support
             raise ValueError('Other detectors not supported, use tf2 type.')
 
-    def detect_next_image(self, image_data):
+    def detect_next_image(self, image_data, input_shape=None):
         """Runs detection on the given image_data.
         :param image_data: Numpy-like array with shape (width, height, n_channels)
+        :param input_shape: Tuple with contents of (input_height, input_width)
         """
-        image_tensor = tf.convert_to_tensor(np.expand_dims(image_data, 0), dtype=tf.uint8)
-        return self.detect_next_tensor(image_tensor)
+        detections = self.Detector.infer_images(image_data, input_shape)
+        logging.info('ElementDetection inference completed.')
+        return detections
 
     def detect_next_tensor(self, image_tensor):
         """Runs detection on the given image_tensor.
@@ -40,12 +42,4 @@ class ElementDetection:
         self.Detector.infer_tensor_input(image_tensor)
         self.output_detection = self.Detector.get_detector_output()
         logging.info('ElementDetection inference completed.')
-        return self.output_detection
-
-    def get_detection_field(self):
-        """Returns an output_detection field of ElementDetection.
-        :return: Dictionary containing the detection output
-        """
-        if self.output_detection is None:
-            raise ValueError('The ElementDetection output is None.')
         return self.output_detection
