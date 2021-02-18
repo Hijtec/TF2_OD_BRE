d8fe7afd82c5213c9b807b0764889a4568a3dcc1
cernil <martin.cernil@ysoft.com>
2021-02-18 12:49:28 +0100

    visualization_utils.py introduced

--- src/utils/visualization_utils.py
+++ src/utils/visualization_utils.py
@@ -1,2 +1,18 @@
-from src.models.research.object_detection.utils.visualization_utils import save_image_array_as_png, \
-    draw_bounding_boxes_on_image_array, visualize_boxes_and_labels_on_image_array
+from PIL import Image
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def __show_image_window(image):
+    image.show()
+
+
+def __show_plt_window(image):
+    plt.imshow(image)
+    plt.show()
+
+
+def show_multiple_images_side_by_side(*images):
+    resulting_image = []
+    for img in images:
+        np.vstack(resulting_image, img)
