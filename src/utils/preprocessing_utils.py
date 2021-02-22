cf1f6dcb7e0e9db74056c59a9489fabdefabe5c2
cernil <martin.cernil@ysoft.com>
2021-02-22 17:16:22 +0100

    preprocessing utils introduced, blur detection

---
+++ src/utils/preprocessing_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+import cv2
+
+
+def detect_blur(image, threshold=100):
+    focus_measure = variance_of_laplacian(image)
+
+    if focus_measure < threshold:
+        return True
+    else:
+        return False
+
+
+def variance_of_laplacian(image):
+    """
+    :param image: ndarray of shape (width, height, channels)
+    :return: float variance of laplacian on that image
+    """
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+    # compute the Laplacian of the image and then return the focus measure, which is simply the variance of the
+    # Laplacian
+
+
+def is_image_blurry(image, threshold=100):
+    return detect_blur(image, threshold)
